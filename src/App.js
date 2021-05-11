0088d0073b7173a1023cee20866e4071d6109a90
boris <github@regmagik.com>
2021-05-11 01:03:45 -0400

    image alt

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ function Contact() {
 			<div>Web: www.ohiophysiciansupport.com</div>
 			</Col>
 			<Col className="md-6">
-				<img className="unsplash" src="https://images.unsplash.com/photo-1560582861-45078880e48e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80"></img>
+				<img className="unsplash" alt="" src="https://images.unsplash.com/photo-1560582861-45078880e48e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1868&q=80"></img>
 			</Col>
 		</Row>
 		</Container>
